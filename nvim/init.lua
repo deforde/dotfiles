aaea30c31d6f3a6b1d47a36702d055059b7f54d0
deforde <daniel.forde001@gmail.com>
2022-03-16 16:44:04 +0200

    Tweaked lsp_signature config.

--- nvim/init.lua
+++ nvim/init.lua
@@ -201,7 +201,11 @@ require'lsp_signature'.setup{
     bind = true, -- This is mandatory, otherwise border config won't get registered.
     handler_opts = {
         border = "rounded"
-    }
+    },
+    hint_prefix = "",
+    floating_window_above_cur_line = false,
+    hint_enable = false,
+    toggle_key = '<C-x>',
 }
 
 vim.opt.tabstop = 4
