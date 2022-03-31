f70aa9ba87935db2959c97dfe93d20a132920a0c
deforde <daniel.forde001@gmail.com>
2022-03-31 16:03:32 +0200

    Revert "Add buffers to lualine."
    This reverts commit 0373fbb88426a8022d57a65397a393296a03663c.

--- nvim/init.lua
+++ nvim/init.lua
@@ -347,38 +347,7 @@ require'nvim-treesitter.configs'.setup {
 
 
 -- Lualine configuration START --
-require('lualine').setup {
-  sections = {
-    lualine_a = {
-      {
-        'buffers',
-        show_filename_only = true,   -- Shows shortened relative path when set to false.
-        show_modified_status = true, -- Shows indicator when the buffer is modified.
-
-        mode = 2, -- 0: Shows buffer name
-                  -- 1: Shows buffer index (bufnr)
-                  -- 2: Shows buffer name + buffer index (bufnr)
-
-        max_length = 100, -- vim.o.columns * 2 / 3, -- Maximum width of buffers component,
-                                            -- it can also be a function that returns
-                                            -- the value of `max_length` dynamically.
-        filetype_names = {
-          TelescopePrompt = 'Telescope',
-          dashboard = 'Dashboard',
-          packer = 'Packer',
-          fzf = 'FZF',
-          alpha = 'Alpha'
-        }, -- Shows specific buffer name for that filetype ( { `filetype` = `buffer_name`, ... } )
-
-        buffers_color = {
-          -- Same values as the general color option can be used here.
-          active = 'lualine_a_normal',     -- Color for active buffer.
-          inactive = 'lualine_a_inactive', -- Color for inactive buffer.
-        },
-      }
-    }
-  }
-}
+require('lualine').setup()
 -- Lualine configuration END --
 
 
