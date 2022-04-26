88cf7d40af805011aefe80712cfd993b8fd79dd8
deforde <daniel.forde001@gmail.com>
2022-04-26 15:49:01 +0200

    Simply Packer bootstrapping.

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,6 +1,13 @@
-local packer = require 'lib.packer-init'
+local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
 
-packer.startup(function(use)
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+  vim.fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
+end
+
+local packer_group = vim.api.nvim_create_augroup('Packer', { clear = true })
+vim.api.nvim_create_autocmd('BufWritePost', { command = 'source <afile> | PackerCompile', group = packer_group, pattern = 'init.lua' })
+
+require("packer").startup(function(use)
   use "wbthomason/packer.nvim"
   use {
       "neovim/nvim-lspconfig",
