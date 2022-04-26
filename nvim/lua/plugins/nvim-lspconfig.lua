586a866284b60060e10473edc0216e050af3ff8d
deforde <daniel.forde001@gmail.com>
2022-04-26 16:14:20 +0200

    Add rust-analyzer to the set of supported LSPs. Other minor tweaks.

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,21 +1,21 @@
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
 
-require'lspconfig'.clangd.setup {
+require("lspconfig").clangd.setup {
     cmd = {
         "clangd",
         "--clang-tidy",
         "-j=8",
         "--compile-commands-dir=./build/",
     },
-    flags = {
-        debounce_text_changes = 150,
-    },
+    capabilities = capabilities,
 }
 
-require'lspconfig'.pyright.setup {
-    flags = {
-        debounce_text_changes = 150,
-    },
+require("lspconfig").pyright.setup {
+    capabilities = capabilities,
+}
+
+require("lspconfig").rust_analyzer.setup {
+    capabilities = capabilities,
 }
 
