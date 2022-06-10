1cc58efc2df9100d7f72bd8c86bfbb98f9bd36f3
deforde <daniel.forde001@gmail.com>
2022-06-10 12:18:56 +0200

    Add nvim-dap-python to plugins.

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -63,6 +63,13 @@ require("packer").startup(function(use)
       end
   }
   use {
+      "mfussenegger/nvim-dap-python",
+      config = function()
+          require"dap-python".setup("~/.virtualenvs/debugpy/bin/python")
+          require"dap-python".test_runner = "pytest"
+      end
+  }
+  use {
       "theHamsta/nvim-dap-virtual-text",
       config = function()
           require"nvim-dap-virtual-text".setup()
