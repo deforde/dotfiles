7b28e351107c1c3bd1a8e97f15625db855187766
deforde <daniel.forde001@gmail.com>
2022-06-19 20:00:06 +0200

    Use default nvim-dap-ui settings.

--- .config/nvim/lua/plugins/nvim-dap-ui.lua
+++ .config/nvim/lua/plugins/nvim-dap-ui.lua
@@ -1,41 +1,4 @@
-require("dapui").setup({
-  icons = { expanded = "▾", collapsed = "▸" },
-  mappings = {
-    expand = { "<CR>", "<2-LeftMouse>" },
-    open = "o",
-    remove = "d",
-    edit = "e",
-    repl = "r",
-    toggle = "t",
-  },
-  sidebar = {
-    elements = {
-      {
-        id = "scopes",
-        size = 0.25,
-      },
-      { id = "breakpoints", size = 0.25 },
-      { id = "stacks", size = 0.25 },
-      { id = "watches", size = 00.25 },
-    },
-    size = 40,
-    position = "left",
-  },
-  tray = {
-    elements = { "repl" },
-    size = 10,
-    position = "bottom",
-  },
-  floating = {
-    max_height = nil,
-    max_width = nil,
-    border = "single",
-    mappings = {
-      close = { "q", "<Esc>" },
-    },
-  },
-  windows = { indent = 1 },
-})
+require("dapui").setup()
 local dap, dapui = require("dap"), require("dapui")
 dap.listeners.after.event_initialized["dapui_config"] = function()
   dapui.open()
