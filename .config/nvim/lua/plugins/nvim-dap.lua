a8c6c69683eadeb67f8139ff75af90654faa7bbf
deforde <daniel.forde001@gmail.com>
2022-09-04 12:18:56 +0200

    use environment variable instead of hard-coded user's dir

--- .config/nvim/lua/plugins/nvim-dap.lua
+++ .config/nvim/lua/plugins/nvim-dap.lua
@@ -3,7 +3,7 @@ local dap = require("dap")
 dap.adapters.cppdbg = {
   id = "cppdbg",
   type = "executable",
-  command = "/home/danielforde/dev/3rdparty/cppdbg/vscode-cpptools/extension/debugAdapters/bin/OpenDebugAD7",
+  command = vim.env.HOME .. "/dev/3rdparty/cppdbg/vscode-cpptools/extension/debugAdapters/bin/OpenDebugAD7",
 }
 dap.configurations.c = {
   {
