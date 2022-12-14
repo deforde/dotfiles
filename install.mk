3c646b6d602a11f2a93ffc5bd3f8303105d8950c
deforde <daniel.forde001@gmail.com>
2022-12-14 15:58:41 +0200

    force kitty symlink

--- install.mk
+++ install.mk
@@ -28,7 +28,7 @@ general:
 
 kitty:
 	curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
-	ln -s $$HOME/.local/kitty.app/bin/kitty $$HOME/.local/bin/
+	ln -f -s $$HOME/.local/kitty.app/bin/kitty $$HOME/.local/bin/
 	cp $$HOME/.local/kitty.app/share/applications/kitty.desktop $$HOME/.local/share/applications/
 	cp $$HOME/.local/kitty.app/share/applications/kitty-open.desktop $$HOME/.local/share/applications/
 	sed -i "s|Icon=kitty|Icon=/home/$$USER/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png|g" $$HOME/.local/share/applications/kitty*.desktop
