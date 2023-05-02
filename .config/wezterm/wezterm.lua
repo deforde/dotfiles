dc3e833b8b7425eb08399dac55cb33c26f00f825
deforde <daniel.forde001@gmail.com>
2023-05-02 14:12:19 +0200

    add wezterm config file

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,35 @@
+local wezterm = require 'wezterm'
+local config = {}
+
+config.default_domain = 'WSL:Ubuntu'
+config.font = wezterm.font 'JetBrains Mono'
+
+config.keys = {
+    {
+        key = 'Enter',
+        mods = 'CTRL',
+        action = wezterm.action.SplitHorizontal{domain='DefaultDomain'},
+    },
+    {
+        key = 'h',
+        mods = 'CTRL',
+        action = wezterm.action.ActivatePaneDirection 'Left',
+    },
+    {
+        key = 'l',
+        mods = 'CTRL',
+        action = wezterm.action.ActivatePaneDirection 'Right',
+    },
+    {
+        key = 'k',
+        mods = 'CTRL',
+        action = wezterm.action.ActivatePaneDirection 'Up',
+    },
+    {
+        key = 'j',
+        mods = 'CTRL',
+        action = wezterm.action.ActivatePaneDirection 'Down',
+    },
+}
+
+return config
