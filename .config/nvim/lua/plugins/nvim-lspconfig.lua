4b61dd7a65bad4280788e5142f0f04540572534a
deforde <daniel.forde001@gmail.com>
2023-05-03 14:26:36 +0200

    add omnisharp lsp for csharp development (including a nasty hack for fixing the broken semantic tokens, see: https://github.com/OmniSharp/omnisharp-roslyn/issues/2483)

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -3,7 +3,7 @@ capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
 local util = require "lspconfig.util"
 
-local servers = { "clangd", "pyright", "rust_analyzer", "zls", "golangci_lint_ls", "gopls", "kotlin_language_server", "lua_ls" };
+local servers = { "clangd", "pyright", "rust_analyzer", "zls", "golangci_lint_ls", "gopls", "kotlin_language_server", "lua_ls", "omnisharp" };
 
 local set_keymap = vim.keymap.set
 local on_attach = function(client, bufnr)
@@ -25,6 +25,82 @@ local on_attach = function(client, bufnr)
     set_keymap("n", "gr", vim.lsp.buf.references, {noremap=true, buffer=bufnr, silent=true, desc="LSP references"})
     set_keymap("n", "<space>fm", vim.lsp.buf.format, {noremap=true, buffer=bufnr, silent=true, desc="LSP formatting"})
     set_keymap("n", "gh", "<cmd>ClangdSwitchSourceHeader<CR>", opts)
+    if client.name == "omnisharp" then
+        client.server_capabilities.semanticTokensProvider = {
+          full = vim.empty_dict(),
+          legend = {
+            tokenModifiers = { "static_symbol" },
+            tokenTypes = {
+              "comment",
+              "excluded_code",
+              "identifier",
+              "keyword",
+              "keyword_control",
+              "number",
+              "operator",
+              "operator_overloaded",
+              "preprocessor_keyword",
+              "string",
+              "whitespace",
+              "text",
+              "static_symbol",
+              "preprocessor_text",
+              "punctuation",
+              "string_verbatim",
+              "string_escape_character",
+              "class_name",
+              "delegate_name",
+              "enum_name",
+              "interface_name",
+              "module_name",
+              "struct_name",
+              "type_parameter_name",
+              "field_name",
+              "enum_member_name",
+              "constant_name",
+              "local_name",
+              "parameter_name",
+              "method_name",
+              "extension_method_name",
+              "property_name",
+              "event_name",
+              "namespace_name",
+              "label_name",
+              "xml_doc_comment_attribute_name",
+              "xml_doc_comment_attribute_quotes",
+              "xml_doc_comment_attribute_value",
+              "xml_doc_comment_cdata_section",
+              "xml_doc_comment_comment",
+              "xml_doc_comment_delimiter",
+              "xml_doc_comment_entity_reference",
+              "xml_doc_comment_name",
+              "xml_doc_comment_processing_instruction",
+              "xml_doc_comment_text",
+              "xml_literal_attribute_name",
+              "xml_literal_attribute_quotes",
+              "xml_literal_attribute_value",
+              "xml_literal_cdata_section",
+              "xml_literal_comment",
+              "xml_literal_delimiter",
+              "xml_literal_embedded_expression",
+              "xml_literal_entity_reference",
+              "xml_literal_name",
+              "xml_literal_processing_instruction",
+              "xml_literal_text",
+              "regex_comment",
+              "regex_character_class",
+              "regex_anchor",
+              "regex_quantifier",
+              "regex_grouping",
+              "regex_alternation",
+              "regex_text",
+              "regex_self_escaped_character",
+              "regex_other_escape",
+            },
+          },
+          range = true,
+        }
+    end
 end
 
 require("mason").setup()
@@ -80,4 +156,9 @@ require("lspconfig").kotlin_language_server.setup {
     capabilities = capabilities,
     on_attach = on_attach,
     root_dir = util.root_pattern("kotlinLspClasspath.sh")
+}
+
+require("lspconfig").omnisharp.setup {
+    capabilities = capabilities,
+    on_attach = on_attach,
 }
